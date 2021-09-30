3a01b06078f35d136cc73fb2b31e7e3bc250e5c8
fearust <fearust@gmail.com>
2021-10-01 06:10:06 +0900

    url modify

--- annport/urls.py
+++ annport/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from wedding import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wedding/', include('wedding.urls')),
+    path('', views.wedding_index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
